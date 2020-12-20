d1ac09471d34e248576c5bd4822f52080ddc93f6
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-12-20 09:42:29 +0200

    Added the initial base JS modules.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Game from './modules/game.js'
+
+window.onload = function() {
+  const game = new Game();
+  game.start();
+}
---
+++ src/modules/game.js
@@ -0,0 +1,13 @@
+export default class Game {
+
+  constructor() {
+    // TODO [sync] initialize subsystems
+  }
+
+  start() {
+    // TODO  [sync] load loading scene
+    // TODO  [sync] show loading scene
+    // TODO [async] load main menu items
+    // TODO  [sync] start main loop
+  }
+}
